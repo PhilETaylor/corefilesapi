ed71da1082bd7a24d9c6bc90f47bbb6c9b91f097
Phil E. Taylor <phil@phil-taylor.com>
2018-09-17 22:56:38 +0100

    progress

---
+++ src/Command/WordpressCommand.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * @copyright  Copyright (C) 2017, 2018 Blue Flame Digital Solutions Limited / Phil Taylor. All rights reserved.
+ * @author     Phil Taylor <phil@phil-taylor.com>
+ *
+ * @see        https://github.com/PhilETaylor/maintain.myjoomla.com
+ *
+ * @license    Commercial License - Not For Distribution.
+ */
+
+namespace App\Command;
+
+use GuzzleHttp\Client;
+use League\Flysystem\Filesystem;
+use League\Flysystem\ZipArchive\ZipArchiveAdapter;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class WordpressCommand extends ContainerAwareCommand
+{
+    /**
+     * @var Guzzle
+     */
+    private $guzzle;
+
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
+    /**
+     * CorefilesCommand constructor.
+     *
+     * @param null|string $name
+     * @param Guzzle $guzzle
+     * @param Crawler $crawler
+     */
+    public function __construct(?string $name = null, Client $guzzle)
+    {
+        $this->guzzle = $guzzle;
+
+        $root = realpath(__DIR__ . '/../../');
+
+        if (!file_exists($root . '/var/tmp/')) {
+            mkdir($root . '/var/tmp/');
+        }
+
+        if (!file_exists($root . '/public/downloads/Wordpress')) {
+            mkdir($root . '/public/downloads/Wordpress');
+        }
+
+        if (!file_exists($root . '/public/downloads/Wordpress/Releases')) {
+            mkdir($root . '/public/downloads/Wordpress/Releases');
+        }
+        if (!file_exists($root . '/public/downloads/Wordpress/Hashes')) {
+            mkdir($root . '/public/downloads/Wordpress/Hashes');
+        }
+        parent::__construct($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('wp:download:all')
+            ->setDescription('Download and hash all wordpress versions')
+            ->setHelp('');
+    }
+
+    /**
+     * This method is executed after interact() and initialize(). It usually
+     * contains the logic to execute to complete this command task.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->output = $output;
+
+        $html = $this->guzzle->get('https://wordpress.org/download/releases/')->getBody()->getContents();
+
+        $crawler = new Crawler($html, '', 'https://wordpress.org/');
+
+        $links = $crawler->filter('a')->links();
+
+        $toDownload = [];
+
+        foreach ($links as $link) {
+
+            if ('zip' !== $link->getNode()->nodeValue) {
+                continue;
+            }
+
+            $downloadLink = $link->getNode()->getAttribute('href');
+
+            if (!$downloadLink || preg_match('/-mu-/ism', $downloadLink)) {
+                continue;
+            }
+
+            $hashFile = $this->generateLocalName($downloadLink);
+
+            if (!file_exists($hashFile)) {
+                $this->doHash($hashFile, $downloadLink);
+            }
+        }
+    }
+
+    private function generateLocalName(string $downloadLink)
+    {
+        $downloadLink = str_replace([
+            '.zip',
+            'https://wordpress.org/wordpress-',
+        ],
+            '',
+            $downloadLink);
+
+        $root = realpath(__DIR__ . '/../../');
+
+        return $root . '/public/downloads/Wordpress/Hashes/' . $downloadLink . '.txt';
+    }
+
+    private function doHash(string $hashFile, string $downloadLink)
+    {
+        $localZip = $this->generateFileName($downloadLink);
+        file_put_contents($localZip, file_get_contents($downloadLink));
+
+        $this->output->writeln('__');
+        $this->output->writeln('<info>Im going to generate md5 hashes based on file ' . $localZip . '</info>');
+
+        $filesystem = new Filesystem(new ZipArchiveAdapter($localZip));
+        $contents = $filesystem->listContents('.', true);
+
+        $this->output->writeln(sprintf('There are %s files to calculate hashes for...', count($contents)));
+
+        $this->output->writeln(sprintf('Im going to save the md5 hashs file as %s', $hashFile));
+
+        // create a new progress bar (50 units)
+        $progress = new ProgressBar($this->output, count($contents));
+
+        $str = '';
+
+        foreach ($contents as $object) {
+
+            if ('file' != $object['type']) {
+                continue;
+            }
+            $filecontents = $filesystem->read($object['path']);
+            $str .= preg_replace('/^wordpress\//', '', $object['path']) . "\t" . hash('md5', $filecontents) . "\n";
+
+            $progress->advance();
+        }
+        $progress->finish();
+
+        file_put_contents($hashFile, $str);
+    }
+
+    private function generateFileName(string $downloadLink)
+    {
+        $downloadLink = str_replace([
+            'https://wordpress.org/',
+        ],
+            '',
+            $downloadLink);
+
+         $root = realpath(__DIR__ . '/../../');
+
+        return $root . '/public/downloads/Wordpress/Releases/' . $downloadLink;
+    }
+}
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,279 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Psr\SimpleCache\InvalidArgumentException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Cache\Simple\FilesystemCache;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Routing\Annotation\Route;
+use ZipArchive;
+
+class DefaultController extends Controller
+{
+
+    const PLATFORM_JOOMLA = 'joomla';
+    const PLATFORM_WORDPRESS = 'wordpress';
+    const FORMAT_RAW = 'raw';
+    const FORMAT_PRETTY = 'pretty';
+    const FORMAT_PRE = 'pre';
+    const FORMAT_FOLDER = 'folder';
+    const FORMAT_MD5 = 'md5';
+    const FORMAT_SHA1 = 'sha1';
+    const FORMAT_STAT = 'stat';
+    const FOLDER = 'FOLDER';
+    const FILE = 'FILE';
+    const IMAGE = 'IMAGE';
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @Route("/")
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function indexAction()
+    {
+        return new JsonResponse([]);
+    }
+
+    /**
+     * @Route("/files")
+     * @return JsonResponse
+     */
+    public function filesAction()
+    {
+
+        $finder = new Finder();
+        $finder->files()->in('./downloads');
+
+        foreach ($finder as $file) {
+            $links[$file->getRelativePath()][] = $file->getRelativePathname();
+        }
+
+        return new JsonResponse($links ?? []);
+    }
+
+    /**
+     * @Route("/files/info")
+     * @Cache(expires="+1 Year", public=true)
+     * @return JsonResponse
+     * @throws InvalidArgumentException
+     */
+    public function filesinfoAction()
+    {
+        $cache = new FilesystemCache();
+
+        if (count($_GET)){
+            $cache->clear();
+        }
+
+        if (!$links = $cache->get('filesinfoAction')) {
+            $finder = new Finder();
+            $finder->files()->in('./downloads');
+
+            foreach ($finder as $file) {
+                if (!preg_match('/Releases/', $file->getRealPath())) {
+                    continue;
+                }
+                $links[$file->getRelativePath()][] = [
+                    'filename' => str_replace([
+                        'Wordpress/Releases/',
+                        'Joomla/Releases/',
+                    ], '', $file->getRelativePathname()),
+                    'md5' => hash('md5', file_get_contents($file->getRealPath())),
+                    'sha1' => hash('sha1', file_get_contents($file->getRealPath())),
+                    'size' => filesize($file->getRealPath()),
+                ];
+            }
+            $cache->set('filesinfoAction', $links, 3600);
+        }
+
+        return new JsonResponse($links ?? []);
+    }
+
+
+    /**
+     * @Route("/{format}/{platform}/{version}/{file}", requirements={"platform"="joomla|wordpress","file"=".+"})
+     * @Cache(expires="+1 Year", public=true)
+     * @param Request $request
+     * @param string $version
+     * @param string $platform
+     * @param string $file
+     * @param string $format
+     * @return Response
+     * @throws Exception
+     */
+    public function mainAction(Request $request, string $version = '', string $platform = '', $file = '', $format = 'raw2')
+    {
+        if (preg_match('/\.\./', $version)) {
+            throw new Exception('Invalid version');
+        }
+
+        switch (strtolower($platform)) {
+            case self::PLATFORM_JOOMLA:
+                $fileToRead = sprintf('Joomla_%s-Stable-Full_Package.zip', $version);
+                $zipFile = realpath('./downloads/Joomla/Releases/') . '/' . $fileToRead;
+                break;
+            case self::PLATFORM_WORDPRESS:
+                $fileToRead = sprintf('wordpress-%s.zip', $version);
+                $zipFile = realpath('./downloads/Wordpress/Releases/') . '/' . $fileToRead;
+                break;
+            default:
+                throw new Exception('Invalid Platform');
+                break;
+        }
+
+        if ("" === $file || preg_match('#/$#', $file)) {
+            $this->type = self::FOLDER;
+            $format = self::FORMAT_FOLDER;
+            $folder = $file;
+        } else {
+            $this->type = self::FILE;
+
+            $path_parts = pathinfo($file);
+
+            if (!array_key_exists('extension', $path_parts)) {
+                $folder = $file;
+                $lang = null;
+            } else {
+                $folder = false;
+                $lang = $path_parts['extension'];
+                switch ($lang) {
+                    case 'gif':
+                        $this->type = self::IMAGE;
+                        $contentType = 'image/gif';
+                        break;
+                    case 'png':
+                        $this->type = self::IMAGE;
+                        $contentType = 'image/png';
+                        break;
+                    case 'jpg':
+                        $this->type = self::IMAGE;
+                        $contentType = 'image/jpeg';
+                        break;
+                    case 'svg':
+                        $this->type = self::IMAGE;
+//                        header('Content-Type: image/jpeg');
+                        break;
+                }
+            }
+        }
+
+
+        if (!file_exists($zipFile)) throw new Exception('Invalid Version To File Mapping: ');
+
+        $zip = new ZipArchive;
+        if (!$zip->open($zipFile)) throw new Exception('Could not open zip file for this version or version invalid');
+
+        if (FALSE !== $folder) {
+            $urls = [];
+            for ($i = 0; $i < $zip->numFiles; $i++) {
+                $name = $zip->getNameIndex($i);
+                if ($platform == self::PLATFORM_WORDPRESS) {
+                    if (preg_match('#^wordpress/' . $folder . '#', $name)) {
+                        $urls[] = preg_replace('/^wordpress\//ism', '', $name);
+                    }
+                } else {
+                    if (preg_match('#^' . $folder . '#', $name)) {
+                        $urls[] = $name;
+                    }
+                }
+            }
+        } else {
+            if ($platform == self::PLATFORM_WORDPRESS) {
+                $file = 'wordpress/' . $file;
+                $txt = $zip->getFromName($file);
+            } else {
+                $txt = $zip->getFromName($file);
+            }
+        }
+
+        if ($format == 'auto' && $this->type == self::FOLDER) {
+            $format = self::FORMAT_FOLDER;
+        } else if ($format == 'auto' && $this->type == self::FILE) {
+            $format = self::FORMAT_PRE;
+        }
+
+        if ($this->type === self::IMAGE) {
+            $response = new Response();
+            $disposition = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_INLINE, basename($file));
+            $response->headers->set('Content-Disposition', $disposition);
+            $response->headers->set('Content-Type', $contentType);
+            $response->setContent($txt);
+
+            return $response;
+
+        }
+
+        switch ($format) {
+            case self::FORMAT_RAW:
+                $template = 'raw.html.twig';
+                break;
+            case self::FORMAT_FOLDER:
+                $template = 'folder.html.twig';
+                break;
+            case self::FORMAT_PRETTY:
+                $txt = htmlentities($txt);
+                $template = 'pretty.html.twig';
+                break;
+            case self::FORMAT_PRE:
+                $txt = htmlentities($txt);
+                $template = 'pre.html.twig';
+                break;
+            case self::FORMAT_FOLDER:
+                $template = 'folder.html.twig';
+                break;
+            case self::FORMAT_SHA1:
+                $hash = hash('sha1', $txt);
+                $template = 'hash.html.twig';
+                break;
+            case self::FORMAT_MD5:
+                $hash = hash('md5', $txt);
+                $template = 'hash.html.twig';
+                break;
+            case self::FORMAT_STAT:
+                if ($this->type == self::FOLDER) {
+                    return new JsonResponse([$urls]);
+                } else {
+                    return new JsonResponse([
+                        'parent' => $fileToRead,
+                        'md5' => hash('md5', $txt),
+                        'sha1' => hash('sha1', $txt),
+                        'base64' => base64_encode($txt),
+                        'strlen' => strlen($txt),
+                    ]);
+                }
+
+                break;
+            default:
+                throw new Exception('Invalid format');
+                break;
+        }
+
+
+        if ('pretty' === $format && $lang == 'js') {
+            $lang = 'javascript';
+        }
+
+        return $this->render($template, [
+            'file' => $fileToRead,
+            'version' => $version,
+            'format' => $format,
+            'platform' => $platform ?? '',
+            'txt' => $txt ?? '',
+            'hash' => $hash ?? '',
+            'lang' => $lang ?? '',
+            'urls' => $urls ?? [],
+        ]);
+    }
+}
